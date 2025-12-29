51164bd32f00eec79fec9fe80deec98a21dd97df
Marcel Šebek <sebek64@post.cz>
2025-12-29 09:26:30 +0100

    Merge high-level and low-level iot clients

--- toshiba-ac-kotlin-client/src/main/kotlin/iot/IoTDeviceClient.kt
+++ toshiba-ac-kotlin-client/src/main/kotlin/iot/IoTDeviceClient.kt
@@ -1,5 +1,12 @@
 package toshibaac.client.iot
 
+import com.google.gson.GsonBuilder
+import com.microsoft.azure.sdk.iot.device.DeviceClient
+import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol
+import com.microsoft.azure.sdk.iot.device.Message
+import com.microsoft.azure.sdk.iot.device.twin.DirectMethodResponse
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.serialization.json.Json
 import toshibaac.client.FCUState
 import toshibaac.client.IncomingSMMobileMethodCall
@@ -7,29 +14,106 @@ import toshibaac.client.IncomingSMMobileMethodCallPayload
 import toshibaac.client.OutgoingMessage
 import toshibaac.client.Temperature
 
-public class HighLevelClient private constructor(
-    private val client: CloseableIoTDeviceClient,
-) : AutoCloseable by client {
+private val log = KotlinLogging.logger {}
+
+public class IoTDeviceClient private constructor(
+    private val client: DeviceClient,
+) : AutoCloseable {
     public companion object {
         public suspend fun create(
             connectionInfo: ConnectionInfo,
             messageCallback: (IncomingSMMobileMethodCall) -> Unit,
-        ): HighLevelClient = HighLevelClient(
-            CloseableIoTDeviceClient.create(
-                connectionInfo = connectionInfo,
-            ) { unparsed ->
-                messageCallback(
-                    unparsed.parse(),
-                )
-            },
-        )
+        ): IoTDeviceClient {
+            val client = DeviceClient(
+                connectionInfo.hostName,
+                connectionInfo.deviceId,
+                {
+                    connectionInfo.sasToken.toCharArray()
+                },
+                IotHubClientProtocol.AMQPS,
+            )
+            client.open(false)
+            val ready = CompletableDeferred<IoTDeviceClient>()
+            client.subscribeToMethodsAsync(
+                { name, payload, _ ->
+                    if (name != "smmobile") {
+                        log.warn { "Unknown method call $name with payload ${payload.payloadAsJsonElement}" }
+                        return@subscribeToMethodsAsync DirectMethodResponse(404, null)
+                    }
+                    val parsedToplevel = payload.getPayload(IncomingSMMobileMethodCallRaw::class.java)
+                    val parsedPayload = when (parsedToplevel.cmd) {
+                        "CMD_SET_SCHEDULE_FROM_AC" -> {
+                            GsonBuilder().create()
+                                .fromJson(parsedToplevel.payload, IncomingSMMobileMethodCallPayloadRaw.SetScheduleFromAC::class.java)
+                        }
+
+                        "CMD_FCU_FROM_AC" -> {
+                            GsonBuilder().create().fromJson(parsedToplevel.payload, IncomingSMMobileMethodCallPayloadRaw.FCUFromAC::class.java)
+                        }
+
+                        "CMD_HEARTBEAT" -> {
+                            GsonBuilder().create().fromJson(parsedToplevel.payload, IncomingSMMobileMethodCallPayloadRaw.Heartbeat::class.java)
+                        }
+
+                        else -> {
+                            log.warn { "Unknown method call payload ${parsedToplevel.cmd}" }
+                            return@subscribeToMethodsAsync DirectMethodResponse(400, null)
+                        }
+                    }
+                    messageCallback(
+                        IncomingSMMobileMethodCallUnparsed(
+                            sourceId = parsedToplevel.sourceId,
+                            messageId = parsedToplevel.messageId,
+                            targetId = parsedToplevel.targetId,
+                            payload = parsedPayload,
+                            timeStamp = parsedToplevel.timeStamp,
+                            timeZone = parsedToplevel.timeZone,
+                            fcuTime = parsedToplevel.fcuTime,
+                        ).parse(),
+                    )
+                    DirectMethodResponse(200, null)
+                },
+                null,
+                { exception, _ ->
+                    if (exception != null) {
+                        ready.completeExceptionally(exception)
+                    }
+                    val deviceClient = IoTDeviceClient(
+                        client = client,
+                    )
+                    ready.complete(deviceClient)
+                },
+                null,
+            )
+            return ready.await()
+        }
     }
 
-    public suspend fun sendMsg(
-        rawMsg: OutgoingMessage,
+    private suspend fun sendMsg(
+        message: OutgoingMessage,
     ) {
-        val msgStr = Json.encodeToString<OutgoingMessage>(rawMsg)
-        client.sendMsg(msgStr)
+        val msgStr = Json.encodeToString<OutgoingMessage>(message)
+        val deferred = CompletableDeferred<Unit>()
+        client.sendEventAsync(
+            Message(msgStr).also { message ->
+                message.contentType = "application/json"
+                message.contentEncoding = "utf-8"
+                message.setProperty("type", "mob")
+            },
+            { _, exception, _ ->
+                if (exception != null) {
+                    log.warn(exception) { "Error sending message $message" }
+                    deferred.completeExceptionally(exception)
+                }
+                deferred.complete(Unit)
+            },
+            null,
+        )
+        deferred.await()
+    }
+
+    override fun close() {
+        client.close()
     }
 }
 
